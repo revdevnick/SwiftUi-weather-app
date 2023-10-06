ddd3f362223054053261953bd82715132cf7ea28
Nick Perkins <nickperkins@protonmail.com>
2023-10-06 11:12:36 -0400

    design change complete

--- SwiftUI-Weather/SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/SwiftUI-Weather/ContentView.swift
@@ -39,91 +39,155 @@ struct ContentView: View {
             ContainerRelativeShape()
                 .fill(Color.sunnyYellow.gradient)
                 .ignoresSafeArea()
-            VStack {
-                Text("Paris")
-                    .font(.sfCompactFont(.bold, size: 26))
-                    .padding(.top, 20)
-                Text("Friday, January 20")
-                    .font(.sfCompactFont(.light, size: 16))
-                    .padding(EdgeInsets(top: 5, leading: 16, bottom: 5, trailing: 16))
-                    .background(.black)
-                    .clipShape(Capsule())
-                    .foregroundColor(.sunnyYellow)
-                Text("Sunny")
-                    .padding()
-                    .font(.sfCompactFont(.medium, size: 20))
-                Text("31°")
-                    .padding()
-                    .font(.sfCompactFont(.regular, size: 250))
-                    .minimumScaleFactor(0.01)
                 VStack(alignment: .leading) {
-                    Text("Daily Summary")
-                        .font(.sfCompactFont(.bold, size: 20))
-                    Text("""
-    Today's weather is partly cloudy with a temperature of 75°. The wind is coming from the northeast at 10mph, and the visbility is 10 miles. Humidity is 40%.
-    """)
-                    .font(.sfCompactFont(.medium, size: 15))
-                    HStack(spacing: 40) {
-                        VStack {
-                            Image(systemName: "wind")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 40, height: 40)
-                                .foregroundColor(.sunnyYellow)
-                                .padding(.top, 8)
-                            Text("4 mph")
-                                .font(.sfCompactFont(.bold, size: 20))
-                                .foregroundColor(.sunnyYellow)
-                                .padding(EdgeInsets(top: 6, leading: 0, bottom: 1, trailing: 0))
-                            Text("Wind")
-                                .font(.sfCompactFont(.light, size: 16))
-                                .foregroundColor(.sunnyYellow)
-                        }
-                        .padding(.vertical, 20)
-                        VStack {
-                            Image(systemName: "drop")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 40, height: 40)
-                                .foregroundColor(.sunnyYellow)
-                                .padding(.top, 8)
-                            Text("48%")
-                                .font(.sfCompactFont(.bold, size: 20))
-                                .foregroundColor(.sunnyYellow)
-                                .padding(EdgeInsets(top: 6, leading: 0, bottom: 1, trailing: 0))
-                            Text("Humidity")
-                                .font(.sfCompactFont(.light, size: 16))
-                                .foregroundColor(.sunnyYellow)
-                        }
-                        .padding(.vertical, 20)
-                        VStack {
-                            Image(systemName: "eye")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 40, height: 40)
-                                .foregroundColor(.sunnyYellow)
-                                .padding(.top, 8)
-                            Text("10.1 mi")
-                                .font(.sfCompactFont(.bold, size: 20))
-                                .foregroundColor(.sunnyYellow)
-                                .padding(EdgeInsets(top: 6, leading: 0, bottom: 1, trailing: 0))
-                            Text("Visbility")
-                                .font(.sfCompactFont(.light, size: 16))
-                                .foregroundColor(.sunnyYellow)
-                        }
-                        .padding(.vertical, 20)
+                    VStack(alignment: .center) {
+                        Text("Paris")
+                            .font(.sfCompactFont(.bold, size: 26))
+                            .padding(.top, 50)
+                        Text("Friday, January 20")
+                            .font(.sfCompactFont(.light, size: 16))
+                            .padding(EdgeInsets(top: 5, leading: 16, bottom: 5, trailing: 16))
+                            .background(.black)
+                            .clipShape(Capsule())
+                            .foregroundColor(.sunnyYellow)
+                            .padding(.top, 8)
+                        Text("Sunny")
+                            .padding(.top, 10)
+                            .font(.sfCompactFont(.medium, size: 20))
+                        Text("31°")
+                            .font(.sfCompactFont(.regular, size: 200))
+                            .padding(.top, -35)
                     }
                     .frame(maxWidth: .infinity)
-                    .background(RoundedRectangle(cornerRadius: 20))
+                    .padding(.bottom, -30)
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text("Daily Summary")
+                            .font(.sfCompactFont(.bold, size: 20))
+                        Text("""
+        Today's weather is partly cloudy with a temperature of 75°. The wind is coming from the northeast at 10mph, and the visbility is 10 miles. Humidity is 40%.
+        """)
+                        .font(.sfCompactFont(.medium, size: 15))
+                        .frame(width: .infinity, height: 72)
+                        .padding(.bottom, 12)
+                        HStack(spacing: 40) {
+                            VStack {
+                                Image(systemName: "wind")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 40, height: 40)
+                                    .foregroundColor(.sunnyYellow)
+                                    .padding(.top, 8)
+                                Text("4 mph")
+                                    .font(.sfCompactFont(.bold, size: 20))
+                                    .foregroundColor(.sunnyYellow)
+                                    .padding(EdgeInsets(top: 6, leading: 0, bottom: 1, trailing: 0))
+                                Text("Wind")
+                                    .font(.sfCompactFont(.light, size: 16))
+                                    .foregroundColor(.sunnyYellow)
+                            }
+                            .padding(.vertical, 20)
+                            VStack {
+                                Image(systemName: "drop")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 40, height: 40)
+                                    .foregroundColor(.sunnyYellow)
+                                    .padding(.top, 8)
+                                Text("48%")
+                                    .font(.sfCompactFont(.bold, size: 20))
+                                    .foregroundColor(.sunnyYellow)
+                                    .padding(EdgeInsets(top: 6, leading: 0, bottom: 1, trailing: 0))
+                                Text("Humidity")
+                                    .font(.sfCompactFont(.light, size: 16))
+                                    .foregroundColor(.sunnyYellow)
+                            }
+                            .padding(.vertical, 20)
+                            VStack {
+                                Image(systemName: "eye")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 40, height: 40)
+                                    .foregroundColor(.sunnyYellow)
+                                    .padding(.top, 8)
+                                Text("10.1 mi")
+                                    .font(.sfCompactFont(.bold, size: 20))
+                                    .foregroundColor(.sunnyYellow)
+                                    .padding(EdgeInsets(top: 6, leading: 0, bottom: 1, trailing: 0))
+                                Text("Visbility")
+                                    .font(.sfCompactFont(.light, size: 16))
+                                    .foregroundColor(.sunnyYellow)
+                            }
+                            .padding(.vertical, 20)
+                        }
+                        .frame(maxWidth: .infinity)
+                        .background(RoundedRectangle(cornerRadius: 10))
+                    }
+                    .padding(.horizontal, 30)
+                    VStack(alignment: .leading) {
+                        Text("3-Day Forecast")
+                            .font(.sfCompactFont(.bold, size: 20))
+                            .padding(.bottom, 5)
+                        HStack(spacing: 20) {
+                            VStack(spacing: 8) {
+                                Text("119°")
+                                    .font(.sfCompactFont(.medium, size: 20))
+                                Image(systemName: "sun.max.fill")
+                                    .resizable()
+                                    .frame(width: 20, height: 20)
+                                Text("Jan 20")
+                                    .font(.sfCompactFont(.medium, size: 15))
+                            }
+                            .padding(.vertical, 10)
+                            .frame(width: 70)
+                            .cornerRadius(10)
+                            .overlay(RoundedRectangle(cornerRadius: 10).stroke(.black, lineWidth: 2))
+                            VStack(spacing: 8) {
+                                Text("119°")
+                                    .font(.sfCompactFont(.medium, size: 20))
+                                Image(systemName: "sun.max.fill")
+                                    .resizable()
+                                    .frame(width: 20, height: 20)
+                                Text("Jan 20")
+                                    .font(.sfCompactFont(.medium, size: 15))
+                            }
+                            .padding(.vertical, 10)
+                            .frame(width: 70)
+                            .cornerRadius(10)
+                            .overlay(RoundedRectangle(cornerRadius: 10).stroke(.black, lineWidth: 2))
+                            VStack(spacing: 8) {
+                                Text("119°")
+                                    .font(.sfCompactFont(.medium, size: 20))
+                                Image(systemName: "sun.max.fill")
+                                    .resizable()
+                                    .frame(width: 20, height: 20)
+                                Text("Jan 20")
+                                    .font(.sfCompactFont(.medium, size: 15))
+                            }
+                            .padding(.vertical, 10)
+                            .frame(width: 70)
+                            .cornerRadius(10)
+                            .overlay(RoundedRectangle(cornerRadius: 10).stroke(.black, lineWidth: 2))
+                            VStack(spacing: 8) {
+                                Text("--°")
+                                    .font(.sfCompactFont(.medium, size: 20))
+                                Image(systemName: "plus.circle")
+                                    .resizable()
+                                    .frame(width: 20, height: 20)
+                                Text("Jan 21")
+                                    .font(.sfCompactFont(.medium, size: 15))
+                            }
+                            .padding(.vertical, 10)
+                            .frame(width: 70)
+                            .cornerRadius(10)
+                            .overlay(RoundedRectangle(cornerRadius: 10).stroke(.black, lineWidth: 2))
+                        }
+                        Spacer()
+                    }
+                    .padding(EdgeInsets(top: 10, leading: 30, bottom: 30, trailing: 30))
+                    
+                    Spacer()
                 }
-                .padding(30)
-                VStack(alignment: .leading) {
-                    Text("Daily Summary")
-                        .font(.sfCompactFont(.bold, size: 20))
-                }
-                Spacer()
             }
-        }
     }
     
     private func oldAppearance() -> some View {
